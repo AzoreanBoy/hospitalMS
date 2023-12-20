185d3d7bf267bd8a806c5aa874806188336ccabe
Pedro <pedroregoferreira@gmail.com>
2023-12-20 17:53:37 +0000

    Exames
    foi desenvolvida a página de mostrar a lista de exames
    foi melhorada algumas coisas da admissao tbm

--- app/urls.py
+++ app/urls.py
@@ -4,31 +4,31 @@ from . import views
 
 urlpatterns = [
     # HOME PAGE for unauthenticated users
-    #path("HMS", views.landing, name="landing"),
+    # path("HMS", views.landing, name="landing"),
 
     # HOME PAGE for authenticated users
     path("", views.index, name="index"),
-    
-    #login/Logout
-    path("login/",views.loginUser, name="login"),
-    path("logout/", views.logoutUser, name="logout"),
-    
-    #Lists of the Entities in the DB
-    path('admissions/',views.admissions, name = 'admissions'),
-    path('physicians/', views.physicians, name='physicians'),
-    path('patients/', views.patients, name ='patients'),
-    path('exams/', views.exams, name = 'exams'),
+
+    # login/Logout
+    path("login/", views.loginUser, name="login"), path("logout/", views.logoutUser, name="logout"),
+
+    # Lists of the Entities in the DB
+    path('admissions/', views.admissions, name='admissions'), path('physicians/', views.physicians, name='physicians'),
+    path('patients/', views.patients, name='patients'), path('exams/', views.exams, name='exams'),
     path('diagnosis/', views.diagnosis, name="diagnosis"),
     path('prescriptions/', views.prescriptions, name='prescriptions'),
-    
+
     # ADMISSIÕES
     path('admissions/<int:id>', views.admisisonDetails, name='admissionDetails'),
     path("newadmission/", views.newAdmission, name="newAdmission"),
-    
+
     # PACIENTES
-    path('patientdetails/<str:pk>', views.patientdetails, name = 'patientdetails'),
+    path('patientdetails/<str:pk>', views.patientdetails, name='patientdetails'),
     path('addpatient/', views.addPatient, name="addPatient"),
-    
+
     # MÉDICOS
     path("physiciandetails/<str:pk>", views.physiciandetails, name="physiciandetails"),
+
+    # Exames
+    path("examdetails/<int:pk>", views.examdetails, name="examdetails"),
 ]
